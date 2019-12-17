a3cefdc0d1811e63351a7ae4dc80f6a207cbe9d1
wannalaughabit <wannalaughabit@yahoo.ca>
2019-12-17 11:38:23 +0100

    creates DTOs for items and orders

--- APIv1/Data Transfer Objects/ItemDto.cs
+++ APIv1/Data Transfer Objects/ItemDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,34 @@ namespace APIv1.Data_Transfer_Objects
 {
     public class ItemDto
     {
+        public string url;
+
+        [JsonProperty("id")]
+        public int? wp_item_id { get; set; }
+
+        [JsonProperty("sku")]
+        public string item_id { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("price")]
+        public int price { get; set; }
+
+        
+
+        public ItemDto()
+        {
+
+        }
+
+        public ItemDto(JObject json)
+        {
+            wp_item_id = (int?)json["id"];
+            item_id = (string)json["sku"];
+            name = (string)json["name"];
+            price = (int)json["price"];            
+        }
+
     }
 }
--- APIv1/Data Transfer Objects/OrderDto.cs
+++ APIv1/Data Transfer Objects/OrderDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,37 @@ namespace APIv1.Data_Transfer_Objects
 {
     public class OrderDto
     {
+        public string url;
+
+        [JsonProperty("id")]
+        public int? wp_order_id { get; set; }    
+        
+
+        [JsonProperty("customer_id")]
+        public int wp_customer_id { get; set; }
+
+        [JsonProperty("total")]
+        public int total { get; set; }
+
+        [JsonProperty("line_items")]
+        public JArray line_items { get; set; }
+
+        [JsonProperty("date_created")]
+        public DateTime date { get; set; }
+
+        public OrderDto()
+        {
+
+        }
+
+        public OrderDto(JObject json)
+        {
+            wp_order_id = (int?)json["id"];
+            wp_customer_id = (int)json["customer_id"];
+            total= (int)json["total"];
+            date = (DateTime)json["date_created"];
+        }
+
     }
+}
 }
